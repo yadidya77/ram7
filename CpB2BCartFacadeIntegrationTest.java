20ad0a791b3ad93331c424321c8bd5cc8e2236bc
yadidya77 <yadidya@gmail.com>
2024-11-29 22:35:28 +0530

    Add files via upload

---
+++ CpB2BCartFacadeIntegrationTest.java
@@ -0,0 +1,34 @@
+/*
+ * HRO 2.0
+ */
+package com.cp.facades.cart;
+
+import de.hybris.bootstrap.annotations.IntegrationTest;
+import de.hybris.platform.servicelayer.ServicelayerTransactionalTest;
+
+import javax.annotation.Resource;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.cp.facades.cart.data.CutoffInfoData;
+
+
+/**
+ * Integration test for CP B2B Cart Facade
+ */
+@IntegrationTest
+public class CpB2BCartFacadeIntegrationTest extends ServicelayerTransactionalTest
+{
+
+	@Resource(name = "cpCartFacade")
+	private CpCartFacade cpCartFacade;
+
+	@Test
+	public void testGetCutOffDateTime()
+	{
+		final CutoffInfoData cutoffInfoData = cpCartFacade.getCutOffDateTime();
+		Assert.assertNotNull(cutoffInfoData);
+	}
+
+}
